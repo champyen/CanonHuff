6686d97518f921aad368d60c0498517ec389805b
Champ Yen <champ.yen@gmail.com>
2020-05-05 23:57:28 +0800

    init implementation, generate code tree and codes

---
+++ huffman.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include "huffman.h"
+
+HuffCoder& HuffCoder::insert(shared_ptr<SymNode> node)
+{
+    symtab[node->symbol] = node;
+    heap->insert(node);
+    numInserted++;
+
+    if(numInserted == numSymbols){
+        heap->makeHeap();
+
+        int left = numSymbols;
+        while(left){
+            shared_ptr<SymNode> min0 = heap->getMin();
+            left = heap->pop(nullptr) - 1;
+            shared_ptr<SymNode> min1 = heap->getMin();
+            shared_ptr<SymNode> node = make_shared<SymNode>(~0UL, min0->kval + min1->kval, min0, min1);
+            if(left)
+                heap->pop(node);
+            root = node;
+        }
+        makeHuffCode(root, 0, 0);
+    }
+
+    return *this;
+}
+
+void HuffCoder::makeHuffCode(shared_ptr<SymNode> node, uint64_t bits, uint64_t code)
+{
+    if(node != nullptr){
+        if(bits > maxLen)
+            maxLen = bits;
+        node->bits = bits;
+        node->code = code;
+        makeHuffCode(node->left, bits+1, code << 1 );
+        makeHuffCode(node->right, bits+1, (code << 1) | 1);
+    }
+}
+
+#ifdef HC_TEST
+#include <stdlib.h>
+#include <time.h>
+
+#include <vector>
+
+int main(void)
+{
+    printf("HuffCoder TEST\n");
+    srand(time(NULL));
+    int numSym = rand()&0xFF;
+    if(numSym < 2) numSym += 0x80;
+    printf("total %d symbols\n", numSym);
+
+    HuffCoder hc(numSym);
+
+    vector<shared_ptr<SymNode>> symlist;
+    for(int i = 0; i < numSym; i++){
+        //printf("%d\n", i);
+        shared_ptr<SymNode> node = make_shared<SymNode>(i, rand()&0xFFFF);
+        symlist.push_back(node);
+        hc.insert(node);
+    }
+    printf("maximum length of code:%d\n", hc.getMaxCodeLen());
+
+    for(int i = 0; i < numSym; i++){
+        shared_ptr<SymNode> node = symlist[i];
+        printf("sym:%lu, freq:%lu, code:%016lX, bits:%lu\n", node->symbol, node->kval, node->code, node->bits);
+    }
+}
+
+#endif
---
+++ huffman.h
@@ -0,0 +1,60 @@
+#ifndef _HUFF_CODER_H_
+#define _HUFF_CODER_H_
+
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+#include <stdint.h>
+
+#include "min_heap.h"
+#include "bitchain.hpp"
+
+class SymNode
+{
+public:
+    SymNode(
+        uint64_t sym,
+        uint64_t freq,
+        shared_ptr<SymNode> lnode = nullptr,
+        shared_ptr<SymNode> rnode = nullptr
+    ){
+        symbol = sym;
+        kval = freq;
+        left = lnode;
+        right = rnode;
+    }
+
+    ~SymNode() {};
+
+    uint64_t symbol = 0;
+    uint64_t kval = 0;       // as freq
+    uint64_t bits = 0;
+    uint64_t code = 0;
+    shared_ptr<SymNode> left;
+    shared_ptr<SymNode> right;
+};
+
+class HuffCoder
+{
+public:
+    HuffCoder(int size) { heap = make_shared<MinHeap<SymNode>>(size); numSymbols = size; }
+    ~HuffCoder() {};
+    HuffCoder& insert(shared_ptr<SymNode>);
+    uint32_t getMaxCodeLen(){ return maxLen; }
+
+    //HuffCoder& encode(uint32_t sym, uint32_t &bits, uint32_t &code, bool dep = true);
+    //HuffCoder& decode(uint32_t &sym, uint32_t code, uint32_t &bits, bool dep = true);
+
+private:
+    void makeHuffCode(shared_ptr<SymNode>, uint64_t, uint64_t);
+
+    unordered_map<uint64_t, shared_ptr<SymNode>> symtab;
+    shared_ptr<MinHeap<SymNode>> heap;
+    shared_ptr<SymNode> root;
+    int numSymbols = 0;
+    int numInserted = 0;
+    int maxLen = 0;
+};
+
+#endif //_CANONICAL_HUFFMAN_H_
